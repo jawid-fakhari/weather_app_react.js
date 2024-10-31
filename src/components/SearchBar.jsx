85e59ba76bb69941b8424c6280e17e75f0a04ae0
jawid-fakhari <jawid_fakhari@live.it>
2024-10-31 14:55:12 +0100

    creare custom hooks per richieste API axios

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,45 +1,20 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import useFetch from '../hooks/useFetch';
+
 
 export default function SearchBar() {
     const [city, setCity] = useState('');
-    const [cityGeoDataLat, setCityGeoDataLat] = useState('');
-    const [cityGeoDataLon, setCityGeoDataLon] = useState('');
-    const [weatherData, setWeatherData] = useState('');
+    const [url, setUrl] = useState(null);
 
-    // API per avere lat & lon della città
-    const geoLocationApi = `https://geocoding-api.open-meteo.com/v1/search?name=${city}&format=json`;
+    // Chiamata a useFetch, che si attiva quando cambia l'URL
+    const { weatherData } = useFetch(url);
 
-
-    const fetchData = async () => {
-        try {
-            // prima fai la richiesta alla geolocation API
-            const geoLocResponse = await axios.get(geoLocationApi)
-
-            // trova le coordinate della città
-            const latitude = geoLocResponse.data.results[0].latitude;
-            const longitude = geoLocResponse.data.results[0].longitude;
-            setCityGeoDataLat(latitude);
-            setCityGeoDataLon(longitude);
-
-            const weatherApi = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m`;
-
-            // ottieni i dati della previsione meteo
-            const weatherResponse = await axios.get(weatherApi);
-            setWeatherData(weatherResponse.data);
-            console.log(weatherResponse.data);
-        } catch (error) {
-            console.error(error);
-        }
-
-
-    }
-
+    // funzione per il submit del form e richiesta dei dati della previsione meteo
     const handleSubmit = (e) => {
         e.preventDefault();
-        fetchData()
-    }
-
+        const newUrl = `https://geocoding-api.open-meteo.com/v1/search?name=${city}&format=json`;
+        setUrl(newUrl); // Imposta il nuovo URL, che attiverà la chiamata in useFetch
+    };
 
     return (
         <>
