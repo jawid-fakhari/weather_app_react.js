6e8b5800a73100be50d74dd0cb038fc241136a44
jawid-fakhari <jawid_fakhari@live.it>
2024-10-31 17:32:44 +0100

    cambio architettura, api nuovo

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,8 +3,6 @@ import axios from 'axios';
 
 export default function SearchBar() {
     const [city, setCity] = useState('');
-    const [cityGeoDataLat, setCityGeoDataLat] = useState('');
-    const [cityGeoDataLon, setCityGeoDataLon] = useState('');
     const [weatherData, setWeatherData] = useState('');
 
     // API per avere lat & lon della città
@@ -19,15 +17,13 @@ export default function SearchBar() {
             // trova le coordinate della città
             const latitude = geoLocResponse.data.results[0].latitude;
             const longitude = geoLocResponse.data.results[0].longitude;
-            setCityGeoDataLat(latitude);
-            setCityGeoDataLon(longitude);
 
-            const weatherApi = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m`;
+            const weatherApi = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,rain,showers,snowfall,cloud_cover,wind_speed_10m&hourly=temperature_2m`;
 
             // ottieni i dati della previsione meteo
             const weatherResponse = await axios.get(weatherApi);
             setWeatherData(weatherResponse.data);
-            console.log(weatherResponse.data);
+
         } catch (error) {
             console.error(error);
         }
@@ -39,6 +35,8 @@ export default function SearchBar() {
         e.preventDefault();
         fetchData()
     }
+
+    console.log(weatherData);
 
 
     return (
@@ -54,6 +52,18 @@ export default function SearchBar() {
                     <button type="submit">Cerca</button>
                 </form>
             </div>
+
+            {weatherData && (
+                <div>
+                    <p>Temperatura: {weatherData.current.temperature_2m}°C</p>
+                    <p>Umidità: {weatherData.current.relative_humidity_2m}</p>
+                    <p>rain: {weatherData.current.rain}</p>
+                    <p>showers: {weatherData.current.showers}</p>
+                    <p>snowfall: {weatherData.current.snowfall}</p>
+                    <p>cloud_cover: {weatherData.current.cloud_cover}</p>
+                    <p>wind_speed_10m: {weatherData.current.wind_speed_10m}</p>
+                </div>
+            )}
         </>
     )
 }
