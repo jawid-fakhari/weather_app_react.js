ce371737b3094be8b2e3d4847b6994d749a828c8
jawid-fakhari <jawid_fakhari@live.it>
2024-11-01 20:31:08 +0100

    creare file Api per le chiamate API

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,43 +1,19 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios';
+import { getCurrentWeather } from '../services/Api';
 
 export default function SearchBar() {
     const [city, setCity] = useState('');
     const [weatherData, setWeatherData] = useState('');
 
-    // API per avere lat & lon della città
-    const geoLocationApi = `https://geocoding-api.open-meteo.com/v1/search?name=${city}&format=json`;
-
-
-    const fetchData = async () => {
-        try {
-            // prima fai la richiesta alla geolocation API
-            const geoLocResponse = await axios.get(geoLocationApi)
-
-            // trova le coordinate della città
-            const latitude = geoLocResponse.data.results[0].latitude;
-            const longitude = geoLocResponse.data.results[0].longitude;
-
-            const weatherApi = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,rain,showers,snowfall,cloud_cover,wind_speed_10m&hourly=temperature_2m`;
-
-            // ottieni i dati della previsione meteo
-            const weatherResponse = await axios.get(weatherApi);
-            setWeatherData(weatherResponse.data);
-
-        } catch (error) {
-            console.error(error);
-        }
-
-
-    }
-
     const handleSubmit = (e) => {
         e.preventDefault();
-        fetchData()
+
+        // fetch weather data usando city name
+        getCurrentWeather({ city })
+            .then(res => {
+                setWeatherData(res);
+            })
     }
-
-    console.log(weatherData);
-
 
     return (
         <>
