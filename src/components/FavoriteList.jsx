b482b21e0401d360f28573b11fed5929cc9403d6
jawid-fakhari <jawid_fakhari@live.it>
2024-11-03 17:21:34 +0100

    sorting function aggiunto

--- src/components/FavoriteList.jsx
+++ src/components/FavoriteList.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function FavoriteList({ favoriteCities, removeFromFavorites }) {
+
+    // state per contenere le città ordinate per temperatura
+    const [sortedCities, setSortedCities] = useState(favoriteCities);
+
+    // inizializza sortedCities con favoritCities ogni volta che favoritCites cambia
+    useEffect(() => {
+        setSortedCities(favoriteCities);
+    }, [favoriteCities]);
+
+    // ordina le città per temperatura in modo decrescente
+    const handleSort = () => {
+        const sorted = [...favoriteCities].sort((a, b) => b.weatherDataInfo.temperatura - a.weatherDataInfo.temperatura);
+        setSortedCities(sorted);
+    }
 
     const deleteHandler = (index) => {
         removeFromFavorites(index)
@@ -11,13 +25,19 @@ export default function FavoriteList({ favoriteCities, removeFromFavorites }) {
             <h3 className="text-xl font-semibold text-gray-800 mb-4">
                 Le tue città Preferite
             </h3>
-            {favoriteCities.length === 0 ? (
+            <button
+                onClick={handleSort}
+                className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg transition duration-200"
+            >
+                Ordina per Temperatura
+            </button>
+            {sortedCities.length === 0 ? (
                 <p className="text-gray-500">
                     Nessuna città preferita aggiunta.
                 </p>
             ) : (
                 <ul className="favCity space-y-4">
-                    {favoriteCities.map((item, index) => (
+                    {sortedCities.map((item, index) => (
                         <li
                             key={index}
                             className="bg-blue-50 p-6 border border-gray-200 rounded-lg shadow-md transition duration-200 hover:shadow-lg"
