c609264aff72e30e0cf4f4283fa0d150672ef860
jawid-fakhari <jawid_fakhari@live.it>
2024-11-29 16:02:41 +0100

    three.js installato per lavorare sul forntend

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { Html, OrbitControls } from "@react-three/drei"
 import Search from "./components/Search"
-import WeatherDisplay from "./components/WeatherDisplay"
+import { Canvas } from "@react-three/fiber"
+import { Suspense } from "react"
+import Background from "./components/Background"
 
 export default function App() {
-  return (
-    <>
-      <Search />
-    </>
-  )
+    return (
+        <Canvas camera={{ position: [0, 0, 5] }} className="">
+            <Suspense>
+                <ambientLight />
+                <OrbitControls />
+                <Background />
+
+                {/* <Html>
+                    <Search />
+                </Html> */}
+            </Suspense>
+        </Canvas>
+    )
 }
